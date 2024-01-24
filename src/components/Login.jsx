2c7ce2cd258aa2963a6b567d30ed920eefa26ede
atharva darvekar <scammer01010@gmail.com>
2024-01-24 13:25:20 +0530

    Atharva 🍉

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
                   </div>
                   <a href="#" class="text-sm font-medium text-white hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
-                <button type="submit" class=" btn-primary w-full text-white bg-indigo-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <button type="submit" class=" btn-primary w-full text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet? <a href="#" class="font-medium text-white hover:underline dark:text-primary-500">Login</a>
                 </p>
